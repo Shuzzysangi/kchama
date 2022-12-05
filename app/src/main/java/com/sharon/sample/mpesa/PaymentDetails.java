0433bacf0518051b0dc0ae3f8b0747328c183873
Kariuki Joseph <kariuki.joseph121@gmail.com>
2022-12-05 08:54:22 +0300

    Client submit payment details

--- app/src/main/java/com/sharon/sample/mpesa/PaymentDetails.java
+++ app/src/main/java/com/sharon/sample/mpesa/PaymentDetails.java
@@ -1,14 +1,98 @@
 package com.sharon.sample.mpesa;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PaymentDetails extends AppCompatActivity {
-
+    EditText edtMpesaCode, edtAmount, edtPurpose;
+    Button btnSubmit;
+    String mPesaCode, purpose;
+    int amount;
+    DatabaseReference paymentsRef;
+    SharedPreferences prefs;
+    FirebaseDatabase database;
+    ProgressDialog loader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_details);
+
+        edtMpesaCode = findViewById(R.id.mPesaCode);
+        edtAmount = findViewById(R.id.amount);
+        edtPurpose = findViewById(R.id.purpose);
+        btnSubmit = findViewById(R.id.submit);
+        database = FirebaseDatabase.getInstance();
+        paymentsRef = database.getReference("/payments");
+        loader = new ProgressDialog(this);
+        loader.setTitle("Posting data");
+        loader.setMessage("Uploading your data... Please wait...");
+        loader.setCancelable(false);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPesaCode = edtMpesaCode.getText().toString().trim();
+                amount = Integer.parseInt(edtAmount.getText().toString().trim());
+                purpose = edtPurpose.getText().toString().trim();
+
+                if(TextUtils.isEmpty(mPesaCode) || TextUtils.isEmpty(edtAmount.getText().toString()) || TextUtils.isEmpty(purpose)){
+                    Toast.makeText(PaymentDetails.this, "Please fill in all the fields.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // get logged in user details
+                String email = prefs.getString("email", null);
+                loader.show();
+                paymentsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        Map<String, String> paymentInfo = new HashMap<>();
+                        paymentInfo.put("mPesaCode", mPesaCode);
+                        paymentInfo.put("amount",String.valueOf(amount));
+                        paymentInfo.put("purpose", purpose);
+
+                        paymentsRef.child(email).setValue(paymentInfo);
+                        loader.dismiss();
+                        Toast.makeText(PaymentDetails.this, "Payment Details submitted successfully.", Toast.LENGTH_SHORT).show();
+
+                        // clear entered user inputs
+                        clearInputs();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        loader.dismiss();
+                    }
+                });
+            }
+        });
+        
+        
+    }
+
+    // clear user inputs
+    public void clearInputs(){
+        edtMpesaCode.setText("");
+        edtAmount.setText("");
+        edtPurpose.setText("");
     }
 }
