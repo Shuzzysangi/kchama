38c6ba4544b0ed885456d9385e11cbae5344fe65
Kariuki Joseph <kariuki.joseph121@gmail.com>
2022-12-07 08:01:14 +0300

    merging with remote

--- app/src/main/java/com/sharon/sample/mpesa/AdminDashboard.java
+++ app/src/main/java/com/sharon/sample/mpesa/AdminDashboard.java
@@ -1,12 +1,16 @@
 package com.sharon.sample.mpesa;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminDashboard extends AppCompatActivity {
 
@@ -14,6 +18,7 @@ public class AdminDashboard extends AppCompatActivity {
     private CardView Reg;
     private CardView wels;
     private CardView Send;
+    private CardView logout;
     private CardView verify;
     private CardView userPayments;
 
@@ -24,6 +29,7 @@ public class AdminDashboard extends AppCompatActivity {
         setContentView(R.layout.activity_admin_dashboard);
         Reg=findViewById(R.id.reg);
        Send=findViewById(R.id.Send);
+        logout=findViewById(R.id.logout);
         verify=findViewById(R.id.verify);
        wels=findViewById(R.id.wels);
         userPayments = findViewById(R.id.userPayments);
@@ -51,6 +57,29 @@ public class AdminDashboard extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(AdminDashboard.this, UserPaymentsActivity.class);
                 startActivity(intent);
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder= new AlertDialog.Builder(AdminDashboard.this);
+                builder.setCancelable(false);
+                builder.setTitle("Exit");
+                builder.setMessage("are you sure you want to logout?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        FirebaseAuth.getInstance().signOut();
+                        startActivity(new Intent(AdminDashboard.this,AdminLogin.class));
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.show();
             }
         });
     }
